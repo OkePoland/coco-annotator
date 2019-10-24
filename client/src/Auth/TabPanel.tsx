2393439f5792d6ab8780aeab5cf05fad309cc7e7
wiola-dreszler <wioletta.dreszler@gmail.com>
2019-10-24 15:27:53 +0200

    Replaced button import

--- client/src/Auth/TabPanel.tsx
+++ client/src/Auth/TabPanel.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
-import { Button } from "@material-ui/core";
+import Button from "@material-ui/core/Button";
 
 import { useStyles } from "./Auth.components";
 
