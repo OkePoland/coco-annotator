323fd507aa6c7652f7fc2ac268b440595e30afed
wiola-dreszler <wioletta.dreszler@gmail.com>
2019-10-24 13:26:10 +0200

    Added Auth view fixes #2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+
+import Auth from "./Auth/Auth";
 
 const App: React.FC = () => {
   return (
     <div>
-      
+      <Auth />
     </div>
   );
-}
+};
 
 export default App;
---
+++ client/src/Auth/Auth.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import Container from "@material-ui/core/Container";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+
+import { useStyles } from "./Auth.components";
+import TabPanel from "./TabPanel";
+
+const Auth: React.FC = () => {
+  const classes = useStyles();
+  const [activeTab, setActiveTab] = useState<number>(0);
+
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setActiveTab(newValue);
+  };
+
+  return (
+    <Container className={classes.container}>
+      <Grid container spacing={3}>
+        <Grid item xs={12} lg={6}>
+          <h1>COCO Annotator</h1>
+          <hr />
+          <div>
+            <h3>You have successfully installed COCO Annotator!</h3>
+            <p>Use the registration form to create an admin account.</p>
+            <p>
+              {"If you have any questions please checkout the "}
+              <Link href="https://github.com/jsbroks/coco-annotator/wiki">
+                wiki
+              </Link>
+              {" before posting "}
+              <Link href="https://github.com/jsbroks/coco-annotator/issues">
+                issues
+              </Link>
+              .
+            </p>
+          </div>
+        </Grid>
+        <Grid item xs={12} lg={6}>
+          <Tabs
+            value={activeTab}
+            onChange={handleChange}
+            TabIndicatorProps={{style: {background: 'transparent'}}}
+          >
+            <Tab className={classes.tab} label="Login" />
+            <Tab className={classes.tab} label="Register" />
+          </Tabs>
+          <TabPanel activeTab={activeTab} index={0}></TabPanel>
+          <TabPanel activeTab={activeTab} index={1}></TabPanel>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+export default Auth;
