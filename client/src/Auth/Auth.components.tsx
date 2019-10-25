4ab7cdf5b54960a80abe1041549e5d3ae79b733e
wiola-dreszler <wioletta.dreszler@gmail.com>
2019-10-25 13:58:18 +0200

    Fixed

--- client/src/Auth/Auth.components.tsx
+++ client/src/Auth/Auth.components.tsx
@@ -1,25 +1,36 @@
-import { makeStyles, Theme } from "@material-ui/core/styles";
+import { makeStyles, createMuiTheme, Theme } from '@material-ui/core/styles';
+
+export const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#007bff',
+        },
+    },
+});
 
 export const useStyles = makeStyles((theme: Theme) => ({
-  form: {
-    width: "100%",
-    marginTop: theme.spacing(1)
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-  container: {
-    display: "flex",
-    flexDirection: "row",
-    padding: 55
-  },
-  tab: {
-    color: "#000",
-    "&:hover": {
-      color: "#007bff"
-    }
-  },
-  label: {
-    backgroundColor: "#FFF"
-  }
+    form: {
+        width: '100%',
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+    container: {
+        display: 'flex',
+        flexDirection: 'row',
+        padding: 55,
+    },
+    tab: {
+        color: theme.palette.primary.dark,
+        '&:hover': {
+            color: theme.palette.primary.main,
+        },
+    },
+    label: {
+        backgroundColor: theme.palette.background.paper,
+    },
+    indicator: {
+        background: theme.palette.background.paper,
+    },
 }));
