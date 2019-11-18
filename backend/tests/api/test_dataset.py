d8d2c3ac722641faef4083860decf7a27d710047
Jakub Korthals <jakub.korthals@oke.pl>
2019-11-18 09:49:45 +0100

    Add and update tests

--- backend/tests/api/test_dataset.py
+++ backend/tests/api/test_dataset.py
@@ -1,5 +1,7 @@
 import json
 import pytest
+from time import sleep
+
 
 class TestDataset:
 
@@ -13,13 +15,22 @@ class TestDataset:
                 inria_id = el['id']
                 return inria_id
 
-    @pytest.mark.skip
+    @staticmethod
+    def _get_inria_info(client, inria_id):
+        url = '/api/dataset/' + str(inria_id) + '/stats'
+        resp = client.get(url)
+        respj = json.loads(resp.data)
+        with open('dat_info', 'w') as f:
+            json.dump(respj, f)
+        return resp, respj
+
+    # @pytest.mark.create
     def test_create_dataset(self, client):
         url = '/api/dataset/'
         data = {"name": "test_inria", "categories": ["person"]}
         resp = client.post(url, json=data)
+        sleep(0.5)
         assert resp.status_code == 200
-
 
     def test_get_datasets(self, client):
         url = '/api/dataset/'
@@ -28,18 +39,49 @@ class TestDataset:
         resp = json.loads(resp.data)
         assert isinstance(resp, list)
 
-
-    @pytest.mark.skip
+    # @pytest.mark.create
     def test_scan_dataset(self, client):
         inria_id = self._get_inria_id(client)
         url = '/api/dataset/' + str(inria_id) + '/scan'
         resp = client.get(url)
+        sleep(0.5)
         assert resp.status_code == 200
+        resp, respj = self._get_inria_info(client, inria_id)
+        assert respj['total']['Images'] == 5
 
-    @pytest.mark.skip
+    # @pytest.mark.create
     def test_import_by_path(self, client):
         inria_id = self._get_inria_id(client)
         url = '/api/dataset/' + str(inria_id) + '/coco'
         data = {'path_string': '/datasets/test_inria'}
         resp = client.post(url, json=data)
+        sleep(2.5)
+        assert resp.status_code == 200
+        resp, respj = self._get_inria_info(client, inria_id)
+        assert respj['total']['Annotated Images'] == 5  # in case of failing this test, try to extend sleeping time
+        assert respj['total']['Annotations'] == 12
+
+    @pytest.mark.skip
+    def test_import_by_folder(self, client):
+        inria_id = self._get_inria_id(client)
+        url = '/api/dataset/' + str(inria_id) + '/coco'
+
+    @pytest.mark.info
+    def test_test(self, client):
+        inria_id = self._get_inria_id(client)
+        resp, respj = self._get_inria_info(client, inria_id)
+        assert respj['total']['Images'] == 5
+        assert respj['total']['Annotated Images'] == 5
+        assert respj['total']['Annotations'] == 12
+        assert resp.status_code == 200
+
+    # @pytest.mark.skip
+    def test_delete(self, client):
+        inria_id = self._get_inria_id(client)
+        url = '/api/dataset/' + str(inria_id)
+        resp = client.delete(url)
+        assert resp.status_code == 200
+        url = '/api/undo/'
+        data = {'id': str(inria_id), 'instance': 'dataset'}
+        resp = client.delete(url, json=data)
         assert resp.status_code == 200
